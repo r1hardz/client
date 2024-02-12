ba73a3d10eb336bf37dc0d37d62dc8b2bbd252c7
r1hardz <120601380+r1hardz@users.noreply.github.com>
2024-02-12 11:34:27 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+
+def receive_messages(client_socket):
+    try:
+        while True:
+            message = client_socket.recv(1024).decode()
+            if not message:
+                break
+            print("Received message:", message)
+    except Exception as e:
+        print("Error receiving messages:", e)
+    finally:
+        client_socket.close()
+
+def send_message(client_socket):
+    try:
+        while True:
+            message = input("Enter your message (type 'exit' to quit): ")
+            if message.lower() == 'exit':
+                break
+            client_socket.sendall(message.encode())
+    except Exception as e:
+        print("Error sending message:", e)
+    finally:
+        client_socket.close()
+
+def start_client():
+    server_address = '51.20.1.254'  # Replace with your server's IP address
+    server_port = 12345  # Replace with your server's port number
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_address, server_port))
+
+    receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+    send_thread = threading.Thread(target=send_message, args=(client_socket,))
+
+    receive_thread.start()
+    send_thread.start()
+
+if __name__ == "__main__":
+    start_client()
